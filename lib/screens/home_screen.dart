88f5aac3977eae2c9f9096f51f552c69612cd2ea
devAdedoyin <isradeoluwa@gmail.com>
2025-02-13 23:58:51 +0000

    BOSE Home Page completed

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -66,7 +66,7 @@ class _HomeScreenState extends State<HomeScreen>
       CurvedAnimation(parent: _spaceController, curve: Curves.easeInOut),
     );
 
-    Future.delayed(const Duration(milliseconds: 1000), () {
+    Future.delayed(const Duration(milliseconds: 600), () {
       if (mounted) _spaceController.forward();
     });
 
