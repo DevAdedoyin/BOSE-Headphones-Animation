eff4d31b3d6c22a27a43c7df8b1552f8a352ed5d
devAdedoyin <isradeoluwa@gmail.com>
2025-02-13 12:59:08 +0000

    Home Screen top icon animation completed

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:bose_ecommerce_animation/constants/colors.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,21 +9,49 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final double _topIconOpacity = 0.5;
+  double _topIconOpacity = 0.1;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Future.delayed(const Duration(milliseconds: 500), (){
+      setState(() {
+        _topIconOpacity = 1.0;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Column(
-        children: [
-          AnimatedOpacity(
-            opacity: _topIconOpacity,
-            duration: const Duration(milliseconds: 500),
-            child: const Row(
-              children: [],
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          children: [
+            AnimatedOpacity(
+              opacity: _topIconOpacity,
+              duration: const Duration(milliseconds: 800),
+              child: const SizedBox(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(
+                      Icons.arrow_back_ios_new,
+                      color: AppColors.primaryColor,
+                    ),
+                    Icon(
+                      Icons.search_rounded,
+                      color: AppColors.primaryColor,
+                    )
+                  ],
+                ),
+              ),
             ),
-          )
-        ],
+
+
+          ],
+        ),
       ),
     );
   }
